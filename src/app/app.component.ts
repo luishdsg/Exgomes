7defc9d094e07edfd978a2717c8b775ef5645a0e
luishdsg <65797541+luishdsg@users.noreply.github.com>
2023-11-29 21:31:32 -0300

    start-a-dream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'exgomes';
+export class AppComponent implements OnInit {
+  constructor() {
+
+  } 
+  ngOnInit(): void {
+  }
 }
